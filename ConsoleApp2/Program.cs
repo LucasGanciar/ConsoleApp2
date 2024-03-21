b23fc472d35710a54c1de7de88a9addef801095a
LucasGanciar <lucas.ganciar@charityjob.co.uk>
2024-03-21 23:38:17 +0000

    sdfgsdf

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,28 +1,46 @@
-﻿namespace GuessNumberGame
+﻿using System.Threading.Tasks.Dataflow;
+
+namespace GuessNumberGame
 {
     class Program
     {
         public static char[] letras = "aáàâãäbcçdeéèêëfghiíìîïjklmnñoóòôõöpqrstuúùûüvwxyzAÁÀÂÃÄBCÇDEÉÈÊËFGHIÍÌÎÏJKLMNÑOÓÒÔÕÖPQRSTUVWXYZ".ToCharArray();
+
         static async Task Main(string[] args)
         {
+            var options = new ExecutionDataflowBlockOptions
+            {
+                MaxDegreeOfParallelism = Environment.ProcessorCount // Adjust the degree of parallelism as needed
+            };
+
+            var actionBlock = new ActionBlock<char>(async letra1 =>
+            {
+                await Task.Run(() =>
+                {
+                    Parallel.ForEach(letras, letra2 =>
+                    {
+                        for (int numero = 1; numero <= 100; numero++)
+                        {
+                            string chave = $"{letra1}{numero}{letra2}";
+                            Console.WriteLine(chave);
+                            _ = EnviarChave(chave);
+                        }
+                    });
+                });
+            }, options);
+
             foreach (var letra1 in letras)
             {
-                foreach (var letra2 in letras)
-                {
-                    for (int numero = 1; numero <= 100; numero++)
-                    {
-                        string chave = $"{letra1}{numero}{letra2}";
-                        Console.WriteLine(chave);
-                        await EnviarChave(chave);
-                    }
-                }
+                await actionBlock.SendAsync(letra1);
             }
+
+            actionBlock.Complete();
+            await actionBlock.Completion;
         }
 
         private static async Task EnviarChave(string chave)
         {
             string endpointUrl = "https://fiap-inaugural.azurewebsites.net/fiap";
-
             string grupo = "grupinho_da_galera_de_portugal";
 
             var jsonBody = new
@@ -37,7 +55,6 @@
                 try
                 {
                     var response = await client.PostAsync(endpointUrl, new StringContent(jsonBodyString, System.Text.Encoding.UTF8, "application/json"));
-
                     Console.WriteLine(response);
                 }
                 catch (Exception ex)
